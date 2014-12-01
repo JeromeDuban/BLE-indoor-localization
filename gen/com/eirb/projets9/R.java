a82caa08c4d28639aa263d41d9913b9effc449dc
cchauvalon <celiachauvalon@hotmail.fr>
2014-12-01 16:11:52 +0100

    Notification element

--- gen/com/eirb/projets9/R.java
+++ gen/com/eirb/projets9/R.java
@@ -83,7 +83,8 @@ public final class R {
         public static final int fragment_notifications=0x7f030005;
         public static final int fragment_planning=0x7f030006;
         public static final int fragment_scan=0x7f030007;
-        public static final int splashscreen=0x7f030008;
+        public static final int notifications_list=0x7f030008;
+        public static final int splashscreen=0x7f030009;
     }
     public static final class menu {
         public static final int main=0x7f0a0000;
