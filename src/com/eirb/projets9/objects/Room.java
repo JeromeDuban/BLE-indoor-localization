06752db67c47e1afba22498c3b7bc1a9394d9a3e
jejefcgb <jduban@enseirb-matmeca.fr>
2015-01-10 15:15:23 +0100

    building parsing done

--- src/com/eirb/projets9/objects/Room.java
+++ src/com/eirb/projets9/objects/Room.java
@@ -9,10 +9,10 @@ public class Room implements Serializable{
 	 */
 	private static final long serialVersionUID = 3916669604411843072L;
 	private int id;
-	private int dom_id;
+	private String dom_id;
 	private String name;
 	
-	public Room(int id, int dom_id, String name) {
+	public Room(int id, String dom_id, String name) {
 		super();
 		this.id = id;
 		this.dom_id = dom_id;
@@ -31,12 +31,12 @@ public class Room implements Serializable{
 		this.id = id;
 	}
 
-	public int getDom_id() {
+	public String getDom_id() {
 		return dom_id;
 	}
 
-	public void setDom_id(int dom_id) {
-		this.dom_id = dom_id;
+	public void setDom_id(String string) {
+		this.dom_id = string;
 	}
 
 	public String getName() {
--- src/com/eirb/projets9/scanner/RangingService.java
+++ src/com/eirb/projets9/scanner/RangingService.java
@@ -33,7 +33,9 @@ import android.os.IBinder;
 import android.os.RemoteException;
 
 import com.eirb.projets9.ReferenceApplication;
+import com.eirb.projets9.objects.Building;
 import com.eirb.projets9.objects.Conference;
+import com.eirb.projets9.objects.Floor;
 import com.eirb.projets9.objects.Room;
 import com.eirb.projets9.objects.Session;
 import com.eirb.projets9.objects.Talk;
@@ -47,6 +49,16 @@ public class RangingService extends Service implements BeaconConsumer, RangeNoti
 	private Region mAllBeaconsRegion;
 	
 	public ArrayList<BeaconRecord> records;
+	
+	private Conference confToSave;
+	private ArrayList<Track> trackList;
+	private ArrayList<Session> sessionList;
+	private ArrayList<Talk> talkList;
+	
+
+	private Building buildToSave;
+	private ArrayList<Floor> floorList;
+	private ArrayList<Room> roomList;
 	
 	@SuppressWarnings("unused")	private BackgroundPowerSaver mBackgroundPowerSaver;
 	@SuppressWarnings("unused")	private RegionBootstrap mRegionBootstrap;
@@ -75,7 +87,6 @@ public class RangingService extends Service implements BeaconConsumer, RangeNoti
 
     }
     
-    @SuppressWarnings({ "unused", "unused" })
 	@Override
     public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
         if (beacons.size() > 0) {
@@ -98,7 +109,7 @@ public class RangingService extends Service implements BeaconConsumer, RangeNoti
             		if (!file.exists()) {
             			System.out.println("Download started");
 
-            			downloadTopology("https://gist.githubusercontent.com/frco9/3670e5e353aadea2c417/raw/7ae27e0ac33388cfbc35107e802feeaf51535c18/Topology_10.json",beacon.getId2().toInt());
+            			downloadBuilding("https://gist.githubusercontent.com/frco9/3670e5e353aadea2c417/raw/7ae27e0ac33388cfbc35107e802feeaf51535c18/Topology_10.json",beacon.getId2().toInt());
 //            			downloadConference("https://gist.githubusercontent.com/frco9/95a6ef89c7d4d4e72c82/raw/cc1684e795566c08103ce87b7841715a45aa5679/Conference_10.json", beacon.getId2().toInt());
             		}
                 }
@@ -156,11 +167,6 @@ public class RangingService extends Service implements BeaconConsumer, RangeNoti
 		
 	}
 
-	private Conference topoToSave;
-	private ArrayList<Track> trackList;
-	private ArrayList<Session> sessionList;
-	private ArrayList<Room> roomList;
-	private ArrayList<Talk> talkList;
 		
 	public boolean isJSONValid(String test) {
 	    try {
@@ -215,7 +221,7 @@ public class RangingService extends Service implements BeaconConsumer, RangeNoti
 				
 				System.out.println("Valid json : " + isJSONValid(json));
 
-				topoToSave = null;
+				confToSave = null;
 
 				try {
 					JSONObject obj = new JSONObject(json);
@@ -298,14 +304,14 @@ public class RangingService extends Service implements BeaconConsumer, RangeNoti
 
 						}
 						conference.setList(trackList);
-						topoToSave = conference;
+						confToSave = conference;
 					}
 
 //					}
 					// Save conference object to file
-					if(topoToSave != null){
-						System.out.println(topoToSave);
-						ReferenceApplication.serializeConference(topoToSave);
+					if(confToSave != null){
+						System.out.println(confToSave);
+						ReferenceApplication.serializeConference(confToSave);
 					}
 					else
 						System.out.println("confToSave == null");
@@ -321,7 +327,7 @@ public class RangingService extends Service implements BeaconConsumer, RangeNoti
 		
 	}
 	
-	public void downloadTopology(String url, int major){
+	public void downloadBuilding(String url, int major){
 		if (isOnline()) {
 			StringBuilder response = new StringBuilder();
 			
@@ -351,96 +357,59 @@ public class RangingService extends Service implements BeaconConsumer, RangeNoti
 			
 			// Parse json
 			if (!response.toString().equals("")) {
-				System.out.println("Writing conference file");
+				System.out.println("Writing building file");
 
 				String json = response.toString();
 				
 				System.out.println("Valid json : " + isJSONValid(json));
 
-				topoToSave = null;
+				buildToSave = null;
 
 				try {
-					JSONObject obj = new JSONObject(json);
-						JSONObject conf = obj;
+					JSONObject build = new JSONObject(json);
+					
+					System.out.println(build.getString("major") +":"+ Integer.toString(major));
+					
+					if (Integer.parseInt(build.getString("major")) == major) {
+						Building building = new Building();
+						building.setMajor(Integer.parseInt(build.getString("major")));
+						building.setName(build.getString("name"));
+						building.setLink(build.getString("link"));
+						 
+						floorList = new ArrayList<Floor>();
+						JSONArray floorsArray = build.getJSONArray("floors");
 						
-						System.out.println(conf.getString("major") +":"+ Integer.toString(major));
-						
-						if (Integer.parseInt(conf.getString("major")) == major) {
-							Conference conference = new Conference();
-
-							conference.setId(Integer.parseInt(conf
-									.getString("id")));
-							conference.setAddress(conf.getString("address"));
-							conference.setTitle(conf.getString("title"));
-							conference.setStartDay(conf.getString("start_day"));
-							conference.setEndDay(conf.getString("end_day"));
-							conference.setMajor(conf.getString("major"));
-							conference.setCreatedAt(Long.parseLong(conf
-									.getString("created_at")));
-							conference.setUpdatedAt(Long.parseLong(conf
-									.getString("updated_at")));
-
-							JSONArray tracks = conf.getJSONArray("tracks");
+						for (int i = 0 ; i < floorsArray.length() ; i++){
 							
-							// Parse tracks
-							trackList = new ArrayList<Track>();
-							for (int j = 0; j < tracks.length(); j++) {
-
-								JSONObject tra = tracks.getJSONObject(j);
-								Track track = new Track();
-								track.setId(Integer.parseInt(tra
-										.getString("id")));
-								track.setTitle(tra.getString("title"));
-
-								JSONArray sessions = tra
-										.getJSONArray("sessions");
+							JSONObject flo = floorsArray.getJSONObject(i);
+							Floor floor = new Floor();
+							floor.setId(Integer.parseInt(flo.getString("id")));
+							floor.setName(flo.getString("name"));
+							
+							roomList = new ArrayList<Room>();
+							JSONArray roomsArray = flo.getJSONArray("rooms");
+							
+							for (int j = 0 ; j < roomsArray.length() ; j++){
 								
-								// Parse sessions
-								sessionList = new ArrayList<Session>();
-								for (int k = 0; k < sessions.length(); k++) {
-
-									JSONObject ses = sessions.getJSONObject(k);
-									Session session = new Session();
-									session.setId(Integer.parseInt(ses.getString("id")));
-									session.setStartTs(Long.parseLong(ses.getString("start_ts")));
-									session.setStartTs(Long.parseLong(ses.getString("end_ts")));
-									session.setRoom_id(Integer.parseInt(ses.getString("room_id")));
-
-									JSONArray talks = ses.getJSONArray("talks");
-									
-									// Parse talks
-									talkList = new ArrayList<Talk>();
-									for (int l = 0; l < talks.length(); l++) {
-
-										JSONObject tal = talks.getJSONObject(l);
-										Talk talk = new Talk();
-										talk.setId(Integer.parseInt(tal.getString("id")));
-										talk.setTitle(tal.getString("title"));
-										talk.setStartTs(Long.parseLong(tal.getString("start_ts")));
-										talk.setEndTs(Long.parseLong(tal.getString("end_ts")));
-										talk.setSpeaker(tal.getString("speaker"));
-										talk.setConfAbstract(tal.getString("abstract"));
-										talk.setBody(tal.getString("abstract"));
-
-										talkList.add(talk);
-									}
-									session.setList(talkList);
-									sessionList.add(session);
-									
-								}
-
-								track.setList(sessionList);
-								trackList.add(track);
-
+								JSONObject roo = roomsArray.getJSONObject(j);
+								Room room = new Room();
+								room.setId(Integer.parseInt(roo.getString("id")));
+								room.setDom_id(roo.getString("dom_id"));
+								room.setName(roo.getString("name"));
+								
+								roomList.add(room);
 							}
-							conference.setList(trackList);
-							topoToSave = conference;
+							floor.setList(roomList);
+							floorList.add(floor);
 						}
+						building.setList(floorList);
+						buildToSave = building;
+					}
 
 					// Save conference object to file
-					if(topoToSave != null){
-						System.out.println(topoToSave);
-						ReferenceApplication.serializeConference(topoToSave);
+					if(buildToSave != null){
+						System.out.println(buildToSave);
+//						ReferenceApplication.serializeConference(buildToSave);
 					}
 					else
 						System.out.println("confToSave == null");
