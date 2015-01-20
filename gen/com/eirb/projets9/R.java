c9171d2ada1e97fe45e0abfbc12ecdfaca4c238b
jejefcgb <jduban@enseirb-matmeca.fr>
2015-01-20 23:45:15 +0100

    hd splashscreen

--- gen/com/eirb/projets9/R.java
+++ gen/com/eirb/projets9/R.java
@@ -57,20 +57,21 @@ public final class R {
         public static final int ic_launcher=0x7f020007;
         public static final int ic_launcher1=0x7f020008;
         public static final int ic_launcher4=0x7f020009;
-        public static final int ic_map=0x7f02000a;
-        public static final int ic_menu_moreoverflow_normal_holo_dark=0x7f02000b;
-        public static final int ic_next=0x7f02000c;
-        public static final int ic_notif=0x7f02000d;
-        public static final int ic_notification=0x7f02000e;
-        public static final int ic_pages=0x7f02000f;
-        public static final int ic_people=0x7f020010;
-        public static final int ic_photos=0x7f020011;
-        public static final int ic_refresh=0x7f020012;
-        public static final int ic_whats_hot=0x7f020013;
-        public static final int list_item_bg_normal=0x7f020014;
-        public static final int list_item_bg_pressed=0x7f020015;
-        public static final int list_selector=0x7f020016;
-        public static final int search=0x7f020017;
+        public static final int ic_launcher_hd=0x7f02000a;
+        public static final int ic_map=0x7f02000b;
+        public static final int ic_menu_moreoverflow_normal_holo_dark=0x7f02000c;
+        public static final int ic_next=0x7f02000d;
+        public static final int ic_notif=0x7f02000e;
+        public static final int ic_notification=0x7f02000f;
+        public static final int ic_pages=0x7f020010;
+        public static final int ic_people=0x7f020011;
+        public static final int ic_photos=0x7f020012;
+        public static final int ic_refresh=0x7f020013;
+        public static final int ic_whats_hot=0x7f020014;
+        public static final int list_item_bg_normal=0x7f020015;
+        public static final int list_item_bg_pressed=0x7f020016;
+        public static final int list_selector=0x7f020017;
+        public static final int search=0x7f020018;
     }
     public static final class id {
         public static final int action_delete=0x7f0c0028;
